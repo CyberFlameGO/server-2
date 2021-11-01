676236890a166f12dabf988a9643d83000efacd2
Thomas Rittson <trittson@bitwarden.com>
2021-11-01 13:45:58 +1000

    Add postgres migrations

---
+++ util/PostgresMigrations/Migrations/20211101031208_CryptoAgent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bit.PostgresMigrations.Migrations
+{
+    public partial class CryptoAgent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UsesCryptoAgent",
+                table: "User",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsesCryptoAgent",
+                table: "User");
+        }
+    }
+}
